544118775333c6e721c6628f199d4653df98d29c
Jesse Szwedko <jesse@szwedko.me>
2020-07-24 17:00:32 -0400

    fixup! Export PATH so it pulls homebrew Ruby first
    Signed-off-by: Jesse Szwedko <jesse@szwedko.me>

--- scripts/environment/bootstrap-macos-10.sh
+++ scripts/environment/bootstrap-macos-10.sh
@@ -3,4 +3,4 @@ set -e -o verbose
 
 brew install ruby
 
-export PATH="/usr/local/opt/ruby/bin:$PATH"' >> "$HOME/.bash_profile"
+echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> "$HOME/.bash_profile"
